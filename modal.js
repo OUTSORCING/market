939f1ec4c43bf81b0f7f7e4d185ec406ad5ec5bf
OUTSORCING <140201844+OUTSORCING@users.noreply.github.com>
2024-03-13 14:40:27 +0000

    Change all

---
+++ modal.js
@@ -0,0 +1,14 @@
+// Показати модальне вікно
+function showModal() {
+    document.getElementById('myModal').style.display = 'block';
+  }
+
+  // Закрити модальне вікно
+  function closeModal() {
+    document.getElementById('myModal').style.display = 'none';
+  }
+
+  // Відкрити модальне вікно при кліку на кнопку
+  function showForm() {
+    showModal();
+  }
